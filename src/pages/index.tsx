58330ce3c8bb183a4b045f6cffb906f397c54e8e
harvinpratama <54679112+Harvin10@users.noreply.github.com>
2023-06-06 13:16:21 +0000

    feat: add new routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import NavigationBar from '../components/navigation-bar/navigation-bar'
 
 export default function ParentPage() {
   return (
     <div>
-      ParentPage
+      <NavigationBar />
       <Outlet />
     </div>
   )
--- src/pages/sorting/Sorting.tsx
+++ src/pages/sorting/Sorting.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function SortingPage() {
   return (
-    <div>SortingPage</div>
+    <section>
+      
+    </section>
   )
 }
